672f586fd5ed279e6cab468851e3c76dc52787f3
CetMendrico <cetmendrico@addu.edu.ph>
2024-10-10 18:27:33 +0800

    Latest nani Renz

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import FilterProductTable from './components/FilterProductTable'
 import ProductTable from './components/ProductTable';
@@ -29,6 +29,19 @@ function App() {
     &&
     (!stockChecked || product.stock > 0)
   );
+
+useEffect(() => {
+  const getProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    console.log(data);
+  };
+
+  getProducts();
+  return () => {
+    
+  }
+}, [])
 
   return (
   <FilterProductTable>
